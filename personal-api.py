02ee1d86fc82525bd2af4962a4c2f948f808b203
22IT034 <22it034@nandhaengg.org>
2025-03-08 09:32:25 +0530

    personal-api.py

--- personal-api.py
+++ personal-api.py
@@ -1,4 +1,18 @@
-Update this file to add your code
-/name - Should give your name
-/register_number - Should give your register_number
-/department - Should give your department
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/name', methods=['GET'])
+def get_name():
+    return "PRIYADHARSHINI"
+
+@app.route('/register_number', methods=['GET'])
+def get_register_number():
+    return "22IT034"
+
+@app.route('/department', methods=['GET'])
+def get_department():
+    return "INFORMATION TECHNOLOGY"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
